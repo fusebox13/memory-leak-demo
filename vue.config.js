dbd4aae8e8d197d164cdbbfaa1b593d85401e260
Big Dan <fusebox13@gmail.com>
2018-11-27 00:19:36 -0500

    Updated webpack config to disable the hot reloader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+let webpack = require("webpack");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  // configureWebpack: {
+  //   plugins: [
+  //     new webpack.LoaderOptionsPlugin({
+  //       test: /\.vue$/,
+  //       options: {
+  //         hotReload: false // disables Hot Reload
+  //       }
+  //     })
+  //   ]
+  // }
+
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.hotReload = false;
+        return options;
+      });
+  }
 };
